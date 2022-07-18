2e15d2e1793668b794ea9c7d6f5d215f411d4cd1
Sergey Ryadchikov <ryadchikov.sa@mail.ru>
2022-07-18 23:10:19 +0300

    Add Task 023

---
+++ Task023/Program.cs
@@ -0,0 +1,11 @@
+﻿void num_cub (int num)
+{
+    for (int x = 1; x <= num; x++)
+    {
+        Console.Write(x * x * x + ", ");
+    }
+}
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+num_cub(N);
