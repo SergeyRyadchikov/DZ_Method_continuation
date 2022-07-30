bed42bbb34852456aa4f6a0fcf4dfe201172f72a
Sergey Ryadchikov <ryadchikov.sa@mail.ru>
2022-07-30 13:25:19 +0300

    add TaskGame and TaskSpiral

---
+++ TaskGame/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Добро подаловать в игру 'Камень, Ножницы, Бумага'!");
+Console.Write("1 - камень" + "\r\n" + "2 - ножницы" + "\r\n" + "3 - бумага" + "\r\n");
+int us1 = 0;
+int us2 = 0;
+int round = 3;
+Random rnd = new Random();
+
+for (int count = 0; count < round; count++)
+{
+    Console.WriteLine("Выберете свой вариант");
+    int user1 = Convert.ToInt32(Console.ReadLine());
+    int user2 = rnd.Next (1, 4);
+    Console.WriteLine(user2);   
+    if (user1 == user2) 
+    {
+        us1++; 
+        us2++;
+    }
+    else if (user1 == 1 && user2 == 2 || user1 == 2 && user2 == 3 || user1 == 3 && user2 == 1) us1++;
+    else us2++;
+    
+}
+Console.WriteLine();
+if (us1 == us2) Console.WriteLine("Вы: " + us1 + "\r\n" + "Противник: " + us2 + "\r\n" + "Победила дружба!");
+else if (us1 < us2) Console.WriteLine("Вы: " + us1 + "\r\n" + "Противник: " + us2 + "\r\n" + "Вы проиграли!");
+else Console.WriteLine("Вы: " + us1 + "\r\n" + "Противник: " + us2 + "\r\n" + "Поздравляем! Вы победили!");
---
+++ TaskSpiral/Program.cs
@@ -0,0 +1,67 @@
+﻿void FillArray(int[,] arr)
+{
+    int finalPosition = arr.GetUpperBound(0) + 1;
+    int startPosition = 0;
+    int start = 1;
+    int i = 0;
+    int j = 0;
+    while (start <= arr.Length)
+    {
+        while (j < finalPosition)
+        {
+            arr[i, j] = start;
+            start++;
+            j++;
+        }
+        i++;
+        while (i < finalPosition)
+        {
+            arr[i, j-1] = start;
+            start++;
+            i++;
+        }
+        j--;
+        while (j > startPosition)
+        {
+            arr[i-1, j-1] = start;
+            start++;
+            j--;
+        }
+        finalPosition--;
+        startPosition++;
+        i = i - 1;
+        while (i > startPosition)
+        {
+            arr[i-1, j] = start;
+            start++;
+            i--;
+        }
+        j++;
+    }
+}
+
+void PrintArray(int[,] coll)
+{
+    int lines = coll.GetUpperBound(0) + 1;
+    int columns = coll.Length / lines;
+    for (int i = 0; i < lines; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(coll[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
+Console.Write("Введите размерность массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[n, n];
+FillArray(array);
+PrintArray(array);
+
+
+
